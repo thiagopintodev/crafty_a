d6681f48783990b41c5372350e7d34e59517dcf8
Thiago Almeida <tapgyn@gmail.com>
2012-07-28 02:09:15 -0300

    faster

--- vendor/assets/javascripts/game.js
+++ vendor/assets/javascripts/game.js
@@ -56,6 +56,16 @@ window.onload = function() {
 				.attr({x: 384, y: i * 16, z: 2});
 		}
 	}
+
+
+
+
+
+
+
+
+
+
 	
 	//the loading screen that will display while our assets load
 	Crafty.scene("loading", function() {
@@ -67,19 +77,30 @@ window.onload = function() {
 		//black background with some loading text
 		Crafty.background("#000");
 		Crafty.e("2D, DOM, Text").attr({w: 100, h: 20, x: 150, y: 120})
-			.text("Loading")
+			.text("Loading...")
 			.css({"text-align": "center"});
 	});
 	
 	//automatically play the loading scene
 	Crafty.scene("loading");
+
+
+
+
+
+
+
+
+
+
+
 	
 	Crafty.scene("main", function() {
 		generateWorld();
 		
 		Crafty.c('CustomControls', {
 			__move: {left: false, right: false, up: false, down: false},	
-			_speed: 1,
+			_speed: 3,
 			
 			CustomControls: function(speed) {
 				if(speed) this._speed = speed;
@@ -101,41 +122,46 @@ window.onload = function() {
 		//create our player entity with some premade components
 		player = Crafty.e("2D, Canvas, player, Controls, CustomControls, Animate, Collision")
 			.attr({x: 160, y: 144, z: 1})
-			.CustomControls(1)
+			.CustomControls(3)
 			.animate("walk_left", 6, 3, 8)
 			.animate("walk_right", 9, 3, 11)
 			.animate("walk_up", 3, 3, 5)
 			.animate("walk_down", 0, 3, 2)
 			.bind("enterframe", function(e) {
-				if(this.isDown("LEFT_ARROW")) {
-					if(!this.isPlaying("walk_left"))
-						this.stop().animate("walk_left", 10);
-				} else if(this.isDown("RIGHT_ARROW")) {
-					if(!this.isPlaying("walk_right"))
-						this.stop().animate("walk_right", 10);
-				} else if(this.isDown("UP_ARROW")) {
-					if(!this.isPlaying("walk_up"))
-						this.stop().animate("walk_up", 10);
-				} else if(this.isDown("DOWN_ARROW")) {
-					if(!this.isPlaying("walk_down"))
-						this.stop().animate("walk_down", 10);
-				}
-			}).bind("keyup", function(e) {
+														if(this.isDown("LEFT_ARROW")) {
+															if(!this.isPlaying("walk_left"))
+																this.stop().animate("walk_left", 3);
+														} else if(this.isDown("RIGHT_ARROW")) {
+															if(!this.isPlaying("walk_right"))
+																this.stop().animate("walk_right", 3);
+														} else if(this.isDown("UP_ARROW")) {
+															if(!this.isPlaying("walk_up"))
+																this.stop().animate("walk_up", 3);
+														} else if(this.isDown("DOWN_ARROW")) {
+															if(!this.isPlaying("walk_down"))
+																this.stop().animate("walk_down", 3);
+														}
+													}
+			).bind("keyup", function(e) {
 				this.stop();
 			})
-			.collision()
-			.onHit("wall_left", function() {
-				this.x += this._speed;
-				this.stop();
-			}).onHit("wall_right", function() {
-				this.x -= this._speed;
-				this.stop();
-			}).onHit("wall_bottom", function() {
-				this.y -= this._speed;
-				this.stop();
-			}).onHit("wall_top", function() {
-				this.y += this._speed;
-				this.stop();
-			});
+			.collision().onHit("wall_left", function() {
+										this.x += this._speed;
+										this.stop();
+									}).onHit("wall_right", function() {
+										this.x -= this._speed;
+										this.stop();
+									}).onHit("wall_bottom", function() {
+										this.y -= this._speed;
+										this.stop();
+									}).onHit("wall_top", function() {
+										this.y += this._speed;
+										this.stop();
+									});
 	});
+
+
+
+
+
 };
